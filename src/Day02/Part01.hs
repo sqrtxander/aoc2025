a798f9941831627b6823c1586e7284e51e14e179
sqrtxander <xander.james128@gmail.com>
2025-12-02 17:01:09 +1100

    Cleaned up day 2, now not using strings in isInvalid

--- src/Day02/Part01.hs
+++ src/Day02/Part01.hs
@@ -7,15 +7,20 @@ parseRange s =
     let ran = splitOn "-" s
      in [read $ head ran .. read $ last ran]
 
-isInvalid :: String -> Bool
-isInvalid istr
-    | odd $ length istr = False
+numLength :: Int -> Int
+numLength 0 = 1
+numLength n = length . takeWhile (> 0) $ iterate (`div` 10) (abs n)
+
+isInvalid :: Int -> Bool
+isInvalid i
+    | odd len = False
     | otherwise =
-        let len = length istr
-            (hi, lo) = splitAt (len `div` 2) istr
+        let (hi, lo) = i `divMod` (10 ^ (len `div` 2))
          in hi == lo
+  where
+    len = numLength i
 
 solution :: String -> Int
-solution s = (sum . map read) (concatMap (filter isInvalid . map show) parsed)
+solution s = sum $ concatMap (filter isInvalid) parsed
   where
     parsed = map parseRange $ splitOn "," s
--- src/Day02/Part02.hs
+++ src/Day02/Part02.hs
@@ -1,25 +1,32 @@
 module Day02.Part02 (solution) where
 
-import Data.List.Split (chunksOf, splitOn)
+import Data.List.Split (splitOn)
 
 parseRange :: String -> [Int]
 parseRange s =
     let ran = splitOn "-" s
      in [read $ head ran .. read $ last ran]
 
-isInvalid :: String -> Bool
-isInvalid = go 1
+numLength :: Int -> Int
+numLength 0 = 1
+numLength n = length . takeWhile (> 0) $ iterate (`div` 10) (abs n)
+
+isInvalid :: Int -> Bool
+isInvalid num = go 1 num
   where
-    go :: Int -> String -> Bool
-    go len istr
-        | len * 2 > length istr = False
-        | otherwise =
-            let cnks = chunksOf len istr
-             in (allTheSame cnks || go (len + 1) istr)
+    len = numLength num
+    go chunkLen i
+        | chunkLen * 2 > len = False
+        | len `mod` chunkLen /= 0 = go (chunkLen + 1) i
+        | otherwise = allTheSame (numChunkify chunkLen i) || go (chunkLen + 1) i
     allTheSame [] = True
     allTheSame (x : xs) = all (== x) xs
+    numChunkify _ 0 = []
+    numChunkify chunkLen i =
+        let (d, m) = i `divMod` (10 ^ chunkLen)
+         in (m : numChunkify chunkLen d)
 
 solution :: String -> Int
-solution s = (sum . map read) (concatMap (filter isInvalid . map show) parsed)
+solution s = sum $ concatMap (filter isInvalid) parsed
   where
     parsed = map parseRange $ splitOn "," s
